3b3b3e1b18799d49ef735a36f4398da0833317cc
yuya-ito-ganbaru <yuya.ito1990@gmail.com>
2023-11-08 11:13:23 +0900

    first commit

---
+++ src/day1/day1.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class day1 {
+    /*
+     * Javaを使った最も基本的なプログラム
+     */
+    public static void main(String[] args) {
+        //「Hello,World」という文字列を表示します。
+        System.out.println("Hello,World");
+        // 数値の表示
+        System.out.print(123);
+        System.out.println(456);
+        // 文字列の表示
+        System.out.print("ABC");
+        System.out.println("DEF");
+    }
+}
