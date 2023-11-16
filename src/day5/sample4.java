c97d60934f05187d886aad4fb7c4fb7a85ce55e1
yuya-ito-ganbaru <yuya.ito1990@gmail.com>
2023-11-16 23:08:22 +0900

    多次元配列

---
+++ src/day5/sample4.java
@@ -0,0 +1,15 @@
+package day5;
+// 配列変数
+public class sample4 {
+    // 多次元配列 不揃いな二次元配列
+    public static void main(String[] args) {
+        int a[][] = { {0,1},{2},{3,4,5,6} };
+        // 成分の表示
+        for(int m = 0; m < a.length; m++) {
+            for(int n = 0; n < a[m].length; n++) {
+                System.out.print(a[m][n] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
