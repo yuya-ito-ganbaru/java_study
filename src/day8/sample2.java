0926e5e610c54277883cbb2e3e4ee980d5c473cf
yuya-ito-ganbaru <yuya.ito1990@gmail.com>
2023-11-18 21:48:02 +0900

    ガーベージコレクタ

--- src/day8/sample2.java
+++ src/day8/sample2.java
@@ -2,11 +2,27 @@ package day8;
 // 静的メンバ
 public class sample2 {
     // ガーベージコレクタ
-    public day8_Class2() {
+    public sample2() {
         System.out.println("コンストラクタ");
     }
     public void foo() {
         System.out.println("インスタンスメソッド");
     }
-    
+    public static void bar() {
+        System.out.println("静的メソッド");
+    }
+    public static void main(String[] args) {
+        // ToDo 自動生成されたメソッド・スタブ
+        sample2 i = new sample2();
+        i.foo();
+        /* 様々な静的メソッドの呼び出し */
+        // インスタンスからも、静的メソッドを呼べる
+        i.bar();
+        // クラス名からの呼び出し
+        sample2.bar();
+        // 同じクラス内なら、クラス名なしで呼び出せる
+        bar();
+        // ガーベージコレクタの呼び出し
+        System.gc();
+    }
 }
